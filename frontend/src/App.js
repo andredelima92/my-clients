53a9317563f5e30f8e2b5394bb0aa89395cc7998
Andre <andredelima92@gmail.com>
2021-03-11 22:05:12 -0300

    react properties

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header title="Titulo 1">
+        <ul>
+          <li>homePage</li>
+          <li>Projeto</li>
+        </ul>
+      </Header>
+      <Header title="Titulo 2" />
+      <Header title="Titulo 3" />
+    </>
+  );
 }
 
 export default App;
